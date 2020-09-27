9cd881a8dcc24c78fb4d99862fec48f1960321ed
Tyler Flores <pre0rdered2@gmail.com>
2020-09-27 15:21:44 -0400

    Tyler Flores - Issues With Interactable Script
    Having issues with my interactable script cause I don't remember how to get proper tags apparently

--- Causation/Assets/_Scripts/Interactables.cs
+++ Causation/Assets/_Scripts/Interactables.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,19 +15,50 @@ public class Interactables : MonoBehaviour
     /// </summary>
 
     //TODO: Create basic collision detector for the following tags:
-    //      Enemy, Object, and Projectile
+    //      Enemy, Object, and Interact
     //Collisions for Object should make it so character cannot move past object without jumping past (if possible) or not at all
     //Collisions for Enemy and Projectile should register hits against player and take away health (Not Included in First Playable)
+    //Collisions for Interact should see if a player is at or near an object such as a sign or door that can be interacted with via the W key
+
+
+
+    //nearObject is supposed to be the trigger object the player has just come into contact with
+    public GameObject nearObject;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //set the tag of nearObject to the tag of the collided item
+        nearObject = collision.GetComponent<GameObject>();
+        nearObject.transform.tag = collision.tag;
+
+        //switch case to determine what to do for each tag
+        switch (nearObject.tag)
+        {
+            case "Enemy":
+                Debug.Log("Object player is at is " + nearObject.tag);
+                break;
+            case "Object":
+                Debug.Log("Object player is at is " + nearObject.tag);
+                break;
+            case "Interact":
+                Debug.Log("Object player is at is " + nearObject.tag);
+                break;
+            default:
+                Debug.Log("Not colliding with proper object");
+                break;
+        }
+
+        Debug.Log(nearObject.tag);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
