1691688b5b844577955f059dea429bf9f1b9387f
Tyler Trumpie <tylertrumpie@gmail.com>
2020-10-05 20:20:55 -0400

    Update Enemy.cs
    Added Flip method, general code fixup

--- Causation/Assets/_Scripts/Enemy.cs
+++ Causation/Assets/_Scripts/Enemy.cs
@@ -7,7 +7,11 @@ public class Enemy : MonoBehaviour
     public int maxHealth = 1;
     public int currentHealth;
 
-    public GameObject enemy;
+    private bool facingRight;
+
+    //public GameObject enemy;
+
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetButtonDown("Fire1"))
         {
             TakeDamage(1);
@@ -28,6 +33,9 @@ public class Enemy : MonoBehaviour
             Death();
         }
 
+        //Controls where the enemy is looking (currently looks at player at all times)
+        Flip();
+
     }
 
     public void TakeDamage(int damage)
@@ -37,6 +45,28 @@ public class Enemy : MonoBehaviour
 
     void Death()
     {
-        Destroy(enemy);
+        //Temporary death, needs animation and drops
+        Destroy(this);
+    }
+
+    //Checks player position and turns to face them
+    private void Flip()
+    {
+        if(player.transform.position.x < this.transform.position.x && !facingRight)
+        {
+            facingRight = true;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+
+            transform.localScale = scale;
+        }
+        else if(player.transform.position.x >= this.transform.position.x && facingRight)
+        {
+            facingRight = false;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+
+            transform.localScale = scale;
+        }
     }
 }
