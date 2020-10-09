6bb30ab59ce66ac15dd4835c3d424bd25349acd6
Tyler Trumpie <tylertrumpie@gmail.com>
2020-10-09 16:26:41 -0400

    Update Enemy.cs
    -Enemy sight range
    -Stops firing when too close

--- Causation/Assets/_Scripts/EnemySpecific/Enemy.cs
+++ Causation/Assets/_Scripts/EnemySpecific/Enemy.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : CharacterBase
 {
+    //Let Trumpie know before making changes
     public Enemy() //constructor
     {
         Health = displayedHealth; //Displayed Health can be set in the inspector
@@ -15,13 +17,17 @@ public class Enemy : CharacterBase
     [Header("Enemy Drops")]
     //Consider making these private and serialized
     public int dropValue;
-    public GameObject drop; //Can drop items or money, just drops money for now
-    public PlayerController player; //this can be private, pretty sure this works now
+    public GameObject drop; //Should drop items or money, just drops money for now
 
+    [Header("Enemy Variables")]
     //Temp Shooting behavior
     public float firerate = 2f;
     private float firerateWait = 0f;
-    bool facingRight;
+    public int sightRange = 10;
+    public int meleeRange = 2;
+
+    private bool facingRight;
+    private PlayerController player; //this can be private, pretty sure this works now
 
     // Start is called before the first frame update
     void Start()
@@ -33,18 +39,47 @@ public class Enemy : CharacterBase
     // Update is called once per frame
     void Update()
     {
+        //Controls where the enemy is looking
+        //First in update to make sure bullet travels in correct direction
+        if (isClose())
+        {
+            Flip(0f);
+        }
+
         //firerateWait changes based on fps time
         firerateWait -= Time.deltaTime;
         //if firerateWait is 0, time to fire and reset the wait
-        if (firerateWait <= 0)
+        if (firerateWait <= 0 && isClose() && !isTooClose())
         {
             Shoot();
             firerateWait = firerate;
         }
 
         ElimCharacter();
-        //Controls where the enemy is looking (currently looks at player at all times)
-        Flip(0f);
+        
+        
+    }
+
+    //isClose and isTooClose are specific to gunslinger enemies, at least currently
+
+    //Checks to see if the player object is within a certain distance
+    private bool isClose()
+    {
+        if(Math.Abs(player.transform.position.x - this.gameObject.transform.position.x) < sightRange)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    //Unused currently, will be implemented with melee support
+    private bool isTooClose()
+    {
+        if (Math.Abs(player.transform.position.x - this.gameObject.transform.position.x) < meleeRange)
+        {
+            return true;
+        }
+        return false;
     }
 
     public override void Flip(float dump) //dump because it doesn't matter but it's needed or errors
