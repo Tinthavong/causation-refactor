09b6f499d0359901d2d067b8810b87e001f2895f
ncowden4 <71358000+ncowden4@users.noreply.github.com>
2020-10-17 17:55:27 -0400

    Medkit Implemetion Done

---
+++ Causation/Assets/_Scripts/UXScripts/Medkit.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Medkit : MonoBehaviour
+{
+    public MedkitController mkc;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            if (mkc.medkitNumber != 3)
+            {
+                switch (mkc.medkitNumber)
+                {
+                    case 0:
+                        {
+                            mkc.medkitNumber += 1;
+                            mkc.medkits[0].SetActive(true);
+                            Debug.Log("Number of Medkits: " + mkc.medkitNumber);
+                            break;
+                        }
+                    case 1:
+                        {
+                            mkc.medkitNumber += 1;
+                            mkc.medkits[1].SetActive(true);
+                            Debug.Log("Number of Medkits: " + mkc.medkitNumber);
+                            break;
+                        }
+                    case 2:
+                        {
+                            mkc.medkitNumber += 1;
+                            mkc.medkits[2].SetActive(true);
+                            Debug.Log("Number of Medkits: " + mkc.medkitNumber);
+                            break;
+                        }
+                }
+
+                AudioSource.PlayClipAtPoint(this.GetComponent<AudioSource>().clip, this.transform.position);
+                Destroy(gameObject);
+            }          
+        }
+    }
+}
---
+++ Causation/Assets/_Scripts/UXScripts/MedkitController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedkitController : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject[] medkits;
+    public HealthBar hb;
+    public CharacterBase chb;
+
+    
+    public int medkitNumber;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            medkits[i].gameObject.SetActive(true);
+        }
+
+        medkitNumber = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && medkitNumber > 0 && chb.displayedHealth != chb.Health)
+        {
+            medkitNumber -= 1;
+            
+            medkits[medkitNumber].gameObject.SetActive(false);
+
+            chb.displayedHealth += 1;
+
+            hb.SetHealth(chb.displayedHealth);
+
+            //Debug.Log("Current Health: " + chb.displayedHealth);
+        }
+    }
+
+    
+}
