3602d94a70cf3b827baaa54c00b46d1260d5f5ef
ncowden4 <71358000+ncowden4@users.noreply.github.com>
2020-12-01 13:35:22 -0500

    GrandDaughter Lvl 2 Elevators Implemented

---
+++ Causation/Assets/ElevatorFlight.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorFlight : MonoBehaviour
+{
+    public GameObject waypointA;
+    public GameObject waypointB;
+    private float speed = 5;
+    public bool shouldChangeFacing = false;
+    private bool directionAB = false;
+
+
+    private void FixedUpdate()
+    {
+        if (this.transform.position ==
+            waypointA.transform.position && directionAB == false
+            || this.transform.position ==
+            waypointB.transform.position && directionAB == true)
+        {
+            directionAB = !directionAB;
+        }
+
+        if (directionAB == true)
+        {
+             this.transform.position =
+             Vector3.MoveTowards(this.transform.position,
+             waypointB.transform.position, speed * Time.fixedDeltaTime);
+        }
+        else
+        {
+             this.transform.position =
+             Vector3.MoveTowards(this.transform.position,
+             waypointA.transform.position, speed * Time.fixedDeltaTime);
+        }
+    }
+
+}
